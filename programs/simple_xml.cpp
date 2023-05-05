df22a3c67cb6770865ec82fa383730726c49123c
Brian Suma <bsumma@tulane.edu>
2023-05-05 15:38:00 -0500

    trying to figure out what is going on with xml and windows

--- programs/simple_xml.cpp
+++ programs/simple_xml.cpp
@@ -11,19 +11,68 @@
 
 using namespace pathCam;
 
+using Poco::AutoPtr;
+using Poco::Util::XMLConfiguration;
+using Poco::Util::LayeredConfiguration;
+
+
+void printSubKeysXML(XMLConfiguration::Ptr config, std::string key, std::string prefix){
+  std::vector<std::string> sub_keys;
+  config->keys(key, sub_keys);
+  std::cout << prefix << "< " << key << " > " << sub_keys.size() << "\n";
+  for(unsigned int i=0; i < sub_keys.size(); i++){
+    std::string new_prefix = "\t" + prefix;
+    printSubKeysXML(config, sub_keys[i], new_prefix);
+  }
+}
+
+void printConfigXML(XMLConfiguration::Ptr config)
+{
+  std::vector<std::string> root_keys;
+  config->keys(root_keys);
+  
+  for(unsigned int i=0; i < root_keys.size(); i++){
+    printSubKeysXML(config, root_keys[i], "" );
+  }
+
+}
+
+
+void printSubKeys(LayeredConfiguration::Ptr config, std::string key, std::string prefix){
+  std::vector<std::string> sub_keys;
+  config->keys(key, sub_keys);
+  std::cout << prefix << "< " << key << " > " << sub_keys.size() << "\n";
+  for(unsigned int i=0; i < sub_keys.size(); i++){
+    std::string new_prefix = "\t" + prefix;
+    printSubKeys(config, sub_keys[i], new_prefix);
+  }
+}
+
+void printConfig(LayeredConfiguration::Ptr config)
+{
+  std::vector<std::string> root_keys;
+  config->keys(root_keys);
+  
+  for(unsigned int i=0; i < root_keys.size(); i++){
+    printSubKeys(config, root_keys[i], "" );
+  }
+
+}
+
+
 
 int main(int argc, char** argv){
 
-  using Poco::AutoPtr;
-  using Poco::Util::XMLConfiguration;
   AutoPtr<XMLConfiguration> pConf(new XMLConfiguration("../../../resources/poco_test.xml"));
-  std::cout <<  pConf->getString("prop1") << std::endl;
-  std::cout <<   pConf->getInt("prop2") << std::endl;
-  std::cout <<   pConf->getString("prop3") << std::endl; // ""
-  std::cout <<   pConf->getString("prop3.prop4") << std::endl; // ""
-  std::cout <<   pConf->getString("prop3.prop4[@attr]") << std::endl; // "value3"
-  std::cout <<   pConf->getString("prop3.prop4[1][@attr]") << std::endl; // "value4"
   
+  pConf->save("../../../resources/output_XML.xml");
+  
+  AutoPtr<LayeredConfiguration> config(new LayeredConfiguration());
+  config->add(pConf);
+  
+  config->save("../../../resources/output_layered.xml");
+
+
   
   return 0;
 }
