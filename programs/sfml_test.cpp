02485d904c56f4e5dc613a1ffdc2e113a34364cf
Brian Summa <bsumma@tulane.edu>
2023-08-21 15:20:43 -0500

    sound threaded

--- programs/sfml_test.cpp
+++ programs/sfml_test.cpp
@@ -10,45 +10,131 @@
 
 using Poco::Path;
 
-
-////////////////////////////////////////////////////////////
-/// Play a sound
-///
-////////////////////////////////////////////////////////////
-void playSound()
-{
+class BeginSound: public Poco::Runnable {
+public:
+  sf::Sound *sound;
+  sf::SoundBuffer *buffer;
   
-  std::stringstream ss;
-  ss <<  PROJECT_SOURCE_DIR << "/resources/stop.wav";
+  BeginSound():sound(0), buffer(0){
+    
+    std::stringstream ss;
+    ss <<  PROJECT_SOURCE_DIR << "/resources/start.wav";
 
-  Poco::Path start_wav_path = Poco::Path(ss.str());
-  
-  // Load a sound buffer from a wav file
-    sf::SoundBuffer buffer;
-    if (!buffer.loadFromFile(start_wav_path.toString()))
-        return;
-
-    // Create a sound instance and play it
-    sf::Sound sound(buffer);
-    sound.play();
-
-    // Loop while the sound is playing
-    while (sound.getStatus() == sf::Sound::Playing)
-    {
-        // Leave some CPU time for other processes
-        sf::sleep(sf::milliseconds(100));
+    Poco::Path start_wav_path = Poco::Path(ss.str());
+    
+    buffer = new sf::SoundBuffer();
+      
+    if (buffer->loadFromFile(start_wav_path.toString())){
+      sound = new sf::Sound(*buffer);
     }
-    std::cout << std::endl << std::endl;
-}
+  }
+
+  ~BeginSound(){
+    if(sound){delete sound;}
+    if(buffer){delete buffer;}
+  }
+  
+  virtual void run(){
+    std::cout << "Hello, world!" << std::endl;
+    if(sound){
+      sound->play();
+      while (sound->getStatus() == sf::Sound::Playing){
+        // Leave some CPU time for other processes
+        Poco::Thread::sleep(100);
+      }
+    }
+  }
+};
+
+class EndSound: public Poco::Runnable {
+public:
+  sf::Sound *sound;
+  sf::SoundBuffer *buffer;
+  
+  EndSound():sound(0), buffer(0){
+    
+    std::stringstream ss;
+    ss <<  PROJECT_SOURCE_DIR << "/resources/stop.wav";
+
+    Poco::Path start_wav_path = Poco::Path(ss.str());
+    
+    buffer = new sf::SoundBuffer();
+      
+    if (buffer->loadFromFile(start_wav_path.toString())){
+      sound = new sf::Sound(*buffer);
+    }
+  }
+
+  ~EndSound(){
+    if(sound){delete sound;}
+    if(buffer){delete buffer;}
+  }
+  
+  virtual void run(){
+    std::cout << "Hello, world!" << std::endl;
+    if(sound){
+      sound->play();
+      while (sound->getStatus() == sf::Sound::Playing){
+        // Leave some CPU time for other processes
+        Poco::Thread::sleep(100);
+      }
+    }
+  }
+};
 
 
-
-int main()
+int main(int argc, char** argv)
 {
-    // Play a sound
-    playSound();
+  BeginSound start;
+  Poco::Thread thread_start;
+  thread_start.start(start);
+  
+  EndSound stop;
+  Poco::Thread thread_stop;
+  thread_stop.start(stop);
 
-    // Wait until the user presses 'enter' key
-    std::cout << "Press enter to exit..." << std::endl;
-    std::cin.ignore(10000, '\n');
+  thread_start.join();
+  thread_stop.join();
+  return 0;
 }
+
+
+
+
+//////////////////////////////////////////////////////////////
+///// Play a sound
+/////
+//////////////////////////////////////////////////////////////
+//void playSound()
+//{
+//
+//  std::stringstream ss;
+//  ss <<  PROJECT_SOURCE_DIR << "/resources/stop.wav";
+//
+//  Poco::Path start_wav_path = Poco::Path(ss.str());
+//
+//  // Load a sound buffer from a wav file
+//    sf::SoundBuffer buffer;
+//    if (!buffer.loadFromFile(start_wav_path.toString()))
+//        return;
+//
+//    // Create a sound instance and play it
+//    sf::Sound sound(buffer);
+//    sound.play();
+//
+//    // Loop while the sound is playing
+//
+//    std::cout << std::endl << std::endl;
+//}
+//
+//
+//
+//int main()
+//{
+//    // Play a sound
+//    playSound();
+//
+//    // Wait until the user presses 'enter' key
+//    std::cout << "Press enter to exit..." << std::endl;
+//    std::cin.ignore(10000, '\n');
+//}
