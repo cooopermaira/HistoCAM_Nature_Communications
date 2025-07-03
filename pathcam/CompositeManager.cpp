48db5e4b9a9cd0afb9a20b4bb10aca15318246ba
cooper maira <cmaira@tulane.edu>
2025-07-03 00:02:15 -0500

    forgot composite mamanger

--- pathcam/CompositeManager.cpp
+++ pathcam/CompositeManager.cpp
@@ -5,6 +5,8 @@
 //  Created by cooper maira on 12/23/23.
 //
 // This class listens for registered frames, sorts them by component membership, then passes them to the component object for compositing.
+//do not change this class unless you REALLY now what youre doing. this class ultimately handles the order of frames being added even if theyre
+//being added by adding a new component. its complicated and fragile.
 
 #include "pathCam.h"
 using namespace cv;
@@ -25,8 +27,11 @@ namespace pathCam {
     rebuildJobsOutstanding = 0;
     while (parent->microscopeInput || parent->diskCount > 0 || parent->regCount > 0 || parent->loaderCount > 0 ||
            parent->matchableCount > 0 || !parent->compositeQ_empty() || !parent->newComponentQ.empty()) {
+
+      debug_termination_check();
+
       //pull new components that might need to be processed
-      std::tuple<unsigned long, cv::Size, unsigned int> newComp;
+      std::tuple<unsigned long, Size, unsigned int> newComp;
       bool isNewComp = false;
       if (!parent->newComponentQ.empty()) {
         newComp = parent->newComponentQ.front();
@@ -36,10 +41,7 @@ namespace pathCam {
       //if nothing in the Q but termination condition not met, wait
       if (parent->compositeQ_empty()) {
         if (isNewComp) {
-          //          perform_global_alignment();
-          //          if (rebuildJobsOutstanding > 0) {
-          //            rebuildJobsComplete.wait();
-          //          }
+
           if (parent->inferencing) {
             push_remaining_tiles_for_inference();
 
@@ -51,17 +53,12 @@ namespace pathCam {
           parent->add_new_component(std::get<0>(newComp), std::get<1>(newComp), std::get<2>(newComp));
           parent->newComponentQ.pop();
         } else {
-          check_render_info();
           Poco::Thread::sleep(100);
         }
       } else {
         auto indexes = parent->get_Q_front();
         if (isNewComp) {
           if (indexes.front()->index > std::get<0>(newComp)) {
-            //perform_global_alignment();
-            if (rebuildJobsOutstanding > 0) {
-              rebuildJobsComplete.wait();
-            }
             parent->add_new_component(std::get<0>(newComp), std::get<1>(newComp), std::get<2>(newComp));
             parent->newComponentQ.pop();
           }
@@ -82,9 +79,12 @@ namespace pathCam {
             new_info.push_back(indexes[i]);
           } else {
             auto start = std::chrono::high_resolution_clock::now();
+
             parent->composites[current_component]->update(new_info);
+
             auto stop = std::chrono::high_resolution_clock::now();
             duration += std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+
             current_component = indexes[i]->component_membership;
             new_info.clear();
           }
@@ -95,7 +95,7 @@ namespace pathCam {
         duration += std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
       }
 
-      check_render_info();
+      //check_render_info();
 
       if (!parent->microscopeInput && parent->loaderCount == 0) {
         submit_outstanding_jobs();
@@ -146,113 +146,39 @@ namespace pathCam {
     }
   }
 
-  void CompositeManager::align_new_comp() {
-    parent->newComponentQ.pop();
-    perform_global_alignment();
-    if (rebuildJobsOutstanding > 0) {
-      rebuildJobsComplete.wait();
+  void CompositeManager::debug_termination_check() {
+    if (parent->matchableCount > 11) {
+      return;
     }
-  }
+    std::vector<unsigned long> emptyList;
+    auto ans = parent->get_image_ref(emptyList);
 
-  void CompositeManager::check_render_info() {
-    for (int i = 1; i < parent->composites.size(); i++) {
-      parent->composites[i]->check_set_render_info();
-    }
-  }
-
-  void CompositeManager::decrement_rebuild_jobs_outstanding() {
-    rebuildJobsOutstanding--;
-    parent->notify_observers();
-    if (rebuildJobsOutstanding == 0) {
-      rebuildJobsComplete.set();
-    }
-  }
-
-  void CompositeManager::perform_SIFT_multires_bundle_adjustment() {
-    std::vector<std::pair<Image*,Image*> > imagePairsToMatch;
-    std::vector<RegInfo*> regs;
-    build_match_pairs(imagePairsToMatch,regs);
-
-    std::vector<MatchesInfo> matchesInfo(imagePairsToMatch.size());
-    for (int i = 0; i < matchesInfo.size(); ++i) {
-      //call params
-      auto sd1 = imagePairsToMatch[i].first->siftData;
-      auto sd2 = imagePairsToMatch[i].second->siftData;
-      auto idx1 = imagePairsToMatch[i].first->index;
-      auto idx2 = imagePairsToMatch[i].second->index;
-
-      matchesInfo[i] = compute_matches_info(sd1, sd2, idx1, idx2);
-    }
-  }
-
-
-  MatchesInfo CompositeManager::compute_matches_info(SiftData &_sift1, SiftData &_sift2, unsigned long _img1_idx, unsigned long _img2_idx) {
-    MatchesInfo matches_info;
-    matches_info.src_img_idx = _img1_idx;
-    matches_info.dst_img_idx = _img2_idx;
-
-    // Run matching in both directions
-    MatchSiftData(_sift1, _sift2);
-    MatchSiftData(_sift2, _sift1);
-
-    // Ensure host-side SiftPoints are synchronized from GPU. need xpos, ypos later for bundle adjustment
-    cudaMemcpy(_sift1.h_data, _sift1.d_data, sizeof(SiftPoint) * _sift1.numPts, cudaMemcpyDeviceToHost);
-    cudaMemcpy(_sift2.h_data, _sift2.d_data, sizeof(SiftPoint) * _sift2.numPts, cudaMemcpyDeviceToHost);
-
-
-    // Track mutual matches
-    for (int i = 0; i < _sift1.numPts; ++i) {
-      int match_idx = _sift1.h_data[i].match;
-      if (match_idx < 0 || match_idx >= _sift2.numPts) continue;
-
-      // Confirm mutual match
-      if (_sift2.h_data[match_idx].match == i) {
-        DMatch m;
-        m.queryIdx = i;
-        m.trainIdx = match_idx;
-        m.distance = _sift1.h_data[i].match_error;
-        matches_info.matches.push_back(m);
+    std::vector<RunnableIntermediate*> somehowOutstandingMatchables;
+    for (auto &img : ans) {
+      auto sojr = parent->JobQ->get_job_ref_index_and_sort_order(2,img->index);
+      if (parent->JobQ->jobRefs.size() <= sojr.first) {
+        return;
+      }
+      auto job = parent->JobQ->jobRefs[sojr.first];
+      if (job && !job->successful) {
+        somehowOutstandingMatchables.push_back(job);
       }
     }
-
-    return matches_info;
-  }
-
-
-  void CompositeManager::build_match_pairs(std::vector<std::pair<Image*, Image*> > &_imagePairsToMatch, std::vector<RegInfo*> &_regs) {
-
-    auto composites = parent->composites;
-    double radSq = pow(0.9 * parent->scope_radius,2);
-
-    for (auto cmp: composites) {
-
-      //get image refs for all delaunay members
-      std::vector<unsigned long> indexes;
-      for (auto item: cmp->delaunayMembers) {
-        indexes.push_back(item.second);
+    std::vector<RunnableIntermediate*> unprocessedJobs,canceledJobs;
+    for (auto &job : somehowOutstandingMatchables) {
+      if (job->unprocessed) {
+        unprocessedJobs.push_back(job);
+      }else {
+        canceledJobs.push_back(job);
       }
-      _regs = parent->get_reg_ref(indexes);
-      auto imgs = parent->get_image_ref(indexes);
-
-      //for each composite, loop thru delaunay members to make pairs within component.
-      //Choose pairs that have a chance of matching
-      for (int i = 0; i < _regs.size(); ++i) {
-        for (int j = i + 1; j < _regs.size(); ++j) {
-          if (cmp->componentMagLabel == Image::_2X) {
-            if (pow(_regs[i]->absoluteCoords.x - _regs[j]->absoluteCoords.x,2) +
-                pow(_regs[i]->absoluteCoords.y - _regs[j]->absoluteCoords.y,2) < radSq) {
-              _imagePairsToMatch.push_back({imgs[i], imgs[j]});
-            }
-          }else {
-            if (abs(_regs[i]->absoluteCoords.x - _regs[j]->absoluteCoords.x) < 0.9 * parent->image_width &&
-                abs(_regs[i]->absoluteCoords.y - _regs[j]->absoluteCoords.y) < 0.9 * parent->image_height) {
-              _imagePairsToMatch.push_back({imgs[i], imgs[j]});
-                }
-          }
-        }
-      }
-      //TODO: create matching pairs across components. this works for initial tests.
     }
+    int k = 0;
   }
+
+
+
+
+
+
 
 }
