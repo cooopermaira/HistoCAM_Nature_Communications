24d53c267708fa402af1e73e2539d3f04b3c911b
pathcam <cmaira@tulane.edu>
2025-12-03 00:53:53 -0600

    more blur work, getting closer

--- programs/bitwise_semi_norm_dot_product.cpp
+++ programs/bitwise_semi_norm_dot_product.cpp
@@ -60,17 +60,111 @@ int extractMagnification(const std::string &path) {
   }
   return -1; // fallback if no match
 }
+void projectKernelNonnegSum1(cv::Mat &k)
+{
+  CV_Assert(k.type() == CV_32F);
 
+  // 1) Clamp negatives to 0
+  cv::max(k, 0.0f, k);  // k = max(k, 0)
 
+  // // 2) Compute sum
+  // double sumK = cv::sum(k)[0];
+  //
+  // // 3) Avoid division by zero: if all zeros, just put a delta at center
+  // if (sumK <= 1e-12) {
+  //   k.setTo(0.0f);
+  //   cv::Point center(k.cols / 2, k.rows / 2);
+  //   k.at<float>(center) = 1.0f;
+  //   return;
+  // }
+  //
+  // // 4) Normalize
+  // k /= static_cast<float>(sumK);
+}
+void applyAlphaToBayer_BGGR(cv::Mat& img32f, const cv::Vec3f& alphaBGR);
+cv::Vec3f computePerChannelScale_BGGR(const cv::Mat &imgA_in,
+                                      const cv::Mat &imgB_in);
+Mat convolveBayerWithKernels_BGGR(
+    const cv::Mat &rawBayerIn,
+    const std::vector<cv::Mat> &kernels  // 3 kernels, CV_32F, same size
+);
+float computeGlobalAlpha(const cv::Mat &A_in, const cv::Mat &B_in);
 
 int main(int argc, char *argv[]) {
+  std::string sharpPath = "/home/pathcam/pcamdata/postProc/blur_test/raw/22.Raw";
+  auto sharpRaw = load_raw(sharpPath);
+
+
+  std::vector<Mat> kernelsBGR;
+  char colors[] = {'B', 'G', 'R'};
+  int patchSize = 2000;
+
+
+  for (int i = 0; i < 3; ++i) {
+    std::string path = "/home/pathcam/pcamdata/postProc/blur_test/train/misc/out/kernel_17_";
+    path.push_back(colors[i]);
+    path += ".tiff";
+
+    kernelsBGR.push_back(imread(path, IMREAD_UNCHANGED));
+    projectKernelNonnegSum1(kernelsBGR[i]);
+  }
+
+  int ksize = kernelsBGR[0].rows;
+  pathCam::Image image(6464 - ksize + 1,4852- ksize + 1,2040);
+  image.set_disk_file("/home/pathcam/pcamdata/postProc/blur_test/train/misc/out/22_convolved_by_channel.Raw");
+  Rect sharpRect((sharpRaw.cols - patchSize) / 2, (sharpRaw.rows - patchSize) / 2,patchSize,patchSize);
+  Rect blurRect((image.width - patchSize) / 2, (image.height - patchSize) / 2, patchSize,patchSize);
+
+  auto img = convolveBayerWithKernels_BGGR(sharpRaw,kernelsBGR);
+
+   auto alphas = computePerChannelScale_BGGR(img(blurRect),sharpRaw(sharpRect));
+  for (int i = 0; i < kernelsBGR.size(); ++i) {
+    kernelsBGR[i] *= alphas[i];
+  }
+
+
+  img = convolveBayerWithKernels_BGGR(sharpRaw,kernelsBGR);
+
+  Mat rcv;
+  img.convertTo(img,CV_8U);
+  cvtColor(img,rcv,COLOR_BayerBG2BGR);
+  imwrite("/home/pathcam/pcamdata/postProc/blur_test/train/misc/out/22_convolved_by_channel_alpha_adjust_kernels.png",rcv);
+  Rect roi(2416 - ksize/2,1422 - ksize/2,image.blurPatch,image.blurPatch);
+
+  //imwrite("/home/pathcam/pcamdata/postProc/blur_test/train/misc/out/22_convolved_by_channel_dft_uhoh.png",image.blurDFT);
+  // assert(img.isContinuous());
+  // size_t t = img.total(),e=img.elemSize();
+  // assert(t * e == image.width * image.height);
+  image.copy_in(img.data);
+  image.check_blur(true);
+  imwrite("/home/pathcam/pcamdata/postProc/blur_test/train/misc/out/22_convolved_by_channel_dft_sum1.png",image.blurDFT);
+  //image.write_to_path();
+  return 0;
+
+  image.load_raw_from_disk();
+  Size image_size(image.width, image.height);
+  Mat img2(image_size,CV_8U,image.get_Raw());
+  //cvtColor(img2,img2,COLOR_BayerBG2BGR);
+  //imwrite("/home/pathcam/pcamdata/postProc/blur_test/train/misc/out/22_convolved_by_channel.png",img2);
+  image.check_blur(true);
+  auto m = image.blurDFT;
+  imwrite("/home/pathcam/pcamdata/postProc/blur_test/train/misc/out/22_convolved_by_channel_dft.png",m);
+
+  image.free_memory_RAW();
+
+
+  int k = 0;
+
+}
+
+int main1(int argc, char *argv[]) {
   std::string blurPath = "/home/pathcam/pcamdata/postProc/blur_test/raw/23.Raw";
   std::string sharpPath = "/home/pathcam/pcamdata/postProc/blur_test/raw/22.Raw";
 
   Size image_size(6464,4852);
 
   auto blurRaw = load_raw(blurPath);
-  char color = 'R';
+  char color = 'G';
   int patchSize = 2000;
   int ksize = 17;
   Rect blurRect((blurRaw.cols - patchSize) / 2,(blurRaw.rows - patchSize) / 2,patchSize,patchSize);
@@ -100,7 +194,7 @@ int main(int argc, char *argv[]) {
 
   sharpRaw(sharpRect).convertTo(source,CV_32F);
   auto kern = estimateKernel_CGLS(source,target,ksize,color,BGGR,1e-5,70,1e-12);
-  //imwrite("/home/pathcam/pcamdata/postProc/blur_test/train/misc/out/kernel_"+std::to_string(ksize)+"_"+color+".tiff",kern);
+  imwrite("/home/pathcam/pcamdata/postProc/blur_test/train/misc/out/kernel_"+std::to_string(ksize)+"_"+color+".tiff",kern);
   auto val = imread("/home/pathcam/pcamdata/postProc/blur_test/train/misc/out/kernel_17_R.tiff",IMREAD_UNCHANGED);
   auto ret = sum(val);
   //normalize(val,val,0,255,NORM_MINMAX,CV_8U);
